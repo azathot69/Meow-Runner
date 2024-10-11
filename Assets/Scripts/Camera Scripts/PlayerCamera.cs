243a1f5d0aa779a88c00d2a99c92a5a17f070d65
azathot69 <jacuna98@gmail.com>
2024-10-10 22:59:40 -0700

    Camera Update
    Camera now has a clamp on both directions; player is unable to move the camera past 180 degrees horizontally nor vertically.

--- Assets/Scripts/Camera Scripts/PlayerCamera.cs
+++ Assets/Scripts/Camera Scripts/PlayerCamera.cs
@@ -29,6 +29,8 @@ public class PlayerCamera : MonoBehaviour
 
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+        yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
         //Rotate Camera and rotation
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
