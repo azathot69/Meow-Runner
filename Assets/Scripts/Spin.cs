6e09fe7a701832fca1e5641f37ff9a981b7d0b20
jdiaz1101 <bearpoker0@gmail.com>
2024-11-05 09:50:33 -0800

    new level

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 spinDirection = Vector3.zero;
+
+    public float spinSpeed = 20f;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rotate the object in the set direction by speed at degrees/second
+        transform.Rotate(spinDirection * spinSpeed * Time.deltaTime);
     }
 }
