d34edfa459559798c13082abf542880975743028
azathot69 <jacuna98@gmail.com>
2024-11-12 22:44:08 -0800

    Teleport Push
    Player will now teleport upon colliding with the cat food can. To change which scene they go to, simply change the can's variable.

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Teleport : MonoBehaviour
+{
+    #region Variables
+    public int goToScene;
+
+    #endregion
+
+    //Go to next scene when colliding with object
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+            GoToScene();
+            Debug.Log("Collided with player");
+        }
+    }
+
+
+    /// <summary>
+    /// Go to Selected scene
+    /// </summary>
+    void GoToScene()
+    {
+        SceneManager.LoadScene(goToScene);
+        Debug.Log("Going to scene");
+    }
+}
