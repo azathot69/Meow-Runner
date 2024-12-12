dee4b7bacafa37faee519efdefccffe667a4a969
azathot69 <jacuna98@gmail.com>
2024-12-11 23:42:33 -0800

    Dodge Push

--- Assets/Scripts/Player Scripts/Dodge.cs
+++ Assets/Scripts/Player Scripts/Dodge.cs
@@ -8,15 +8,27 @@ public class Dodge : MonoBehaviour
     [SerializeField]
     private bool canDodge;
 
-    public float dodgeCoolDownTimer;
-    public float dodgeCoolDown;
+    public float dodgeSpeed;
+
     public KeyCode dodgeAction = KeyCode.Mouse1;
 
+    public float dodgeCooldown;
+
+    private float countDown;
+    public float countDownMax;
+    public bool stratCountdown;
+
+    [Header("References")]
+    private PlayerScript pm;
+    private Rigidbody rb;
+    public Transform orientation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerScript>();
+        countDown = countDownMax;
     }
 
     // Update is called once per frame
@@ -25,32 +37,59 @@ public class Dodge : MonoBehaviour
         DodgeInput();
     }
 
+    private void FixedUpdate()
+    {
+        if (stratCountdown)
+        {
+            //Timer
+            if (countDown > 0) countDown -= Time.deltaTime;
+            if (countDown <= 0)
+            {
+                canDodge = false;
+
+                countDown = countDownMax;
+
+                stratCountdown = false;
+
+                //Reset Variables
+                Invoke(nameof(DodgeReset), dodgeCooldown);
+            }
+        }
+    }
+
     /// <summary>
     /// Get Dodge Input
     /// </summary>
     private void DodgeInput()
     {
-        if (Input.GetKey(dodgeAction) && canDodge)
+        if (Input.GetKeyDown(dodgeAction) && canDodge)
         {
-            canDodge = false;
-
+            stratCountdown = true;
             PlayerDodge();
-
-            Invoke(nameof(DodgeReset), dodgeCoolDown);
         }
     }
 
     private void PlayerDodge()
     {
         //Get Player Invincible Variable
+        pm.invincible = true;
+        rb.useGravity = false;
 
-        //Get player input
+        
 
         //Move Player
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(rb.velocity * dodgeSpeed, ForceMode.Impulse);
+
+        
+
+        
     }
 
     private void DodgeReset()
     {
+        pm.invincible = false;
+        rb.useGravity = true;
         canDodge = true;
     }
 }
